ba4b5b71f0689ff3fa91f2501ae7bd42291a8245
Jose <Jose@Usuario-PC>
2016-03-04 10:03:40 +0100

    eo

--- pruebaGIT/src/pruebaGIT/pruebaGIT.java
+++ pruebaGIT/src/pruebaGIT/pruebaGIT.java
@@ -5,6 +5,8 @@ public class pruebaGIT {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo");
+		System.out.println("Adios mundo");
+		System.out.println("Hola");
 
 	}
 
